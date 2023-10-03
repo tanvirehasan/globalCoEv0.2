dc534fa56e82b1b1a4e70b37a1977d8946b51391
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-10-03 19:06:06 +0600

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -32,14 +32,14 @@
 <body>
 
     <!-- preloader -->
-    <!-- <div id="preloader">
+    <div id="preloader">
         <div id="loading-center">
             <div class="loader">
                 <div class="loader-outter"></div>
                 <div class="loader-inner"></div>
             </div>
         </div>
-    </div> -->
+    </div>
     <!-- preloader-end -->
 
     <!-- Scroll-top -->
